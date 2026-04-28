4cc4c75b2a85d2d4c315ab130b9bc2dd3b9450c5
Ravi Tarun N <rtpn2000@gmail.com>
2026-04-27 18:13:13 -0700

    Updated the config with the NEBULA ENDPOINT.

--- config.py
+++ config.py
@@ -9,6 +9,9 @@ load_dotenv()
 # API Keys.
 metalprice_api_key = os.getenv("METALPRICE_KEY")
 goldapi_key = os.getenv("GOLDAPI_KEY")
+nebula_api_key = os.getenv("NEBULA_API_KEY")
+nebula_api_url = os.getenv("NEBULA_API_URL", "https://workspace.nebulaapi.com/gold_price")
+nebula_city = os.getenv("NEBULA_CITY", "Hyderabad")
 
 # Constants.
 ounce_to_g = 31.1034768
